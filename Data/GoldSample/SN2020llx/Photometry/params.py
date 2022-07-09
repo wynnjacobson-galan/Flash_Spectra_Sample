de15c6cb5c764715b1da289e30b4b864a4b79cda
Wynn Jacobson-Galan <wjacobso@ucsc.edu>
2022-07-08 22:49:21 -0500

    20llx

---
+++ Data/GoldSample/SN2020llx/Photometry/params.py
@@ -0,0 +1,35 @@
+texp = 58998.51 
+ebvmw = 0.02
+ebvh = 0.0 
+dSN = 30.160 #+/- 6.5*
+SNname = 'SN2020llx'
+vwind = 50
+SNshift = 0
+pherr = 1.5
+chirange = 20
+z = 0.009947
+tlines = 10.
+etexp = 0.6
+SP = 10
+
+ymin = -12
+ymax = -19
+
+xmin = -4
+xmax = 50
+
+HA_BG = 0.01
+HB_BG = 0.01
+HG_BG = 0.01
+HeII_NIII_BG = 0.018
+HeII_BG = 0.01
+CIV_BG = 0.001
+NIV_BG = 0.002
+
+HA_D = 0.01
+HB_D = 0.01
+HG_D = 0.01
+HeII_NIII_D = 0.018
+HeII_D = 0.01
+CIV_D = 0.01
+NIV_D = 0.02
