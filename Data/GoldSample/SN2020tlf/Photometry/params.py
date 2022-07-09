96dbc57588d96b3195cb08b797a5bedd4779eec7
Wynn Jacobson-Galan <wjacobso@ucsc.edu>
2022-07-08 22:38:20 -0500

    20tlf

---
+++ Data/GoldSample/SN2020tlf/Photometry/params.py
@@ -0,0 +1,35 @@
+texp = 59095.25
+ebvmw = 0.014
+ebvh = 0.018 
+dSN = 36.482
+SNname = 'SN2020tlf'
+vwind = 50
+SNshift = 0
+pherr = 0.5
+chirange = 20
+z = 0.00838532
+tlines = 20.
+etexp = 0.5
+SP = 3
+
+ymin = -13.8
+ymax = -19.4
+
+xmin = -4
+xmax = 74
+
+HA_BG = 0.01
+HB_BG = 0.01
+HG_BG = 0.01
+HeII_NIII_BG = 0.018
+HeII_BG = 0.01
+CIV_BG = 0.001
+NIV_BG = 0.002
+
+HA_D = 0.01
+HB_D = 0.01
+HG_D = 0.01
+HeII_NIII_D = 0.018
+HeII_D = 0.01
+CIV_D = 0.01
+NIV_D = 0.02
